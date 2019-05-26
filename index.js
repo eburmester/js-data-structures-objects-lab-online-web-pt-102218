a63b543433fdd5a2475fab95c624ddb44eb113bf
Elyse Burmester <burmesterelyse@gmail.com>
2019-05-26 19:13:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,15 @@ function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
   return obj;
 }
 
+function deleteFromDriverByKey(obj, key) {
+  deleteDriver = Object.assign({}, obj)
+  delete deleteDriver[key]
 
+  return deleteDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+  delete obj[key];
+
+  return obj;
+}
