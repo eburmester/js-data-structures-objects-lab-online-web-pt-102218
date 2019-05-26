b1dd0cc4078e33f22ce585acf4e503d8266951ba
Elyse Burmester <burmesterelyse@gmail.com>
2019-05-26 19:12:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const driver = {};
 
+function updateDriverWithKeyAndValue(obj, key, value) {
+  newDriver = Object.assign({}, obj);
+  newDriver[key] = value
+
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+
+  return obj;
+}
+
 
